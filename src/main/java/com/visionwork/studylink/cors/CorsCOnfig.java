aa1c67b6b77b2e9372264cef734b1e31d4968bb2
Gustavo <gustavodossantos111@hotmail.com>
2024-09-22 16:43:14 -0300

    autorizacao

--- src/main/java/com/visionwork/studylink/cors/CorsCOnfig.java
+++ src/main/java/com/visionwork/studylink/cors/CorsCOnfig.java
@@ -1,2 +1,16 @@
-package com.visionwork.studylink.cors;public class CorsCOnfig {
+package com.visionwork.studylink.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsCOnfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST");
+    }
 }
